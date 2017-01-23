7137d93a012c623c0d74934587594ca4062180ba
Crazy Steven <956995511@qq.com>
2017-01-23 19:00:29 +0800

    Second commit

---
+++ UploadCrash/CrashLogUpdate.h
@@ -0,0 +1,13 @@
+//
+//  CrashLogUpdate.h
+//  UploadCrash
+//
+//  Created by Steven on 2017/1/23.
+//  Copyright © 2017年 Steven. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CrashLogUpdate : NSObject
+
+@end
---
+++ UploadCrash/CrashLogUpdate.m
@@ -0,0 +1,13 @@
+//
+//  CrashLogUpdate.m
+//  UploadCrash
+//
+//  Created by Steven on 2017/1/23.
+//  Copyright © 2017年 Steven. All rights reserved.
+//
+
+#import "CrashLogUpdate.h"
+
+@implementation CrashLogUpdate
+
+@end
